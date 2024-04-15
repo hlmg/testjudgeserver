2fbf7c1f88446ef2766b187a56c86da26c4d4a31
hlmg <devhlmg@gmail.com>
2024-04-15 20:22:54 +0900

    Initial commit

---
+++ src/main/java/com/example/testjudgeserver/TestjudgeserverApplication.java
@@ -0,0 +1,13 @@
+package com.example.testjudgeserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestjudgeserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestjudgeserverApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/testjudgeserver/TestjudgeserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testjudgeserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestjudgeserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
